9c0419d95de5008c688933807d55951a18c20e66
pramodhosahalli <pramodhosahalli@gmail.com>
2021-02-27 16:21:39 +0530

    Holds Controller and service definition

---
+++ static/js/exampleApp.js
@@ -0,0 +1,86 @@
+//
+var exampleApp = angular.module('exampleApp',[]);
+
+exampleApp.service('ajaxService', function ($http,$q) {
+    
+    this.ajaxHttpGetCall = function (url) {
+        var deferred = $q.defer();
+        $http.get(url)
+        .then(function(response) {
+             deferred.resolve(response);
+        },function(response){
+            deferred.resolve(response);
+        });
+        return deferred.promise
+    };
+
+    this.ajaxHttpPostCall = function (url,data) {
+        var deferred = $q.defer();
+        $http.post(url,data)
+        .then(function(response) {
+             deferred.resolve(response);
+        },function(response){
+            deferred.resolve(response);
+        });
+        return deferred.promise
+    };
+});
+
+exampleApp.controller('tableController', function($scope, ajaxService){
+    
+    $scope.isEditMode = false;
+    $scope.checkboxList = [];
+    
+    $scope.getImages = function(){
+        var result = ajaxService.ajaxHttpGetCall("./getImages")
+        result.then(function(response){
+            $scope.tableContents = response.data;
+        });
+    };
+
+    $scope.uploadImages = function(){
+        var result = ajaxService.ajaxHttpPostCall("./uploadImages",$scope.form)
+        result.then(function(response){
+            $scope.form = {} // reset the form contents
+            $scope.getImages(); // call get Images to fetch list of updated images
+        });
+    };
+
+    //From the Table Contents, get the Objects to be Deleted and Send it to BackEnd..
+    $scope.deleteImages = function(){
+        var objectsToBeDeleted = {}
+        var countChanges = 0;
+        for(index in $scope.checkboxList){
+            var objectId = $scope.checkboxList[index]
+            if(objectId > 0)
+            {
+                countChanges+=1;
+                objectsToBeDeleted[objectId] = true
+            }
+        }
+
+        if(countChanges == 0){
+            //No changes made, just delete button pressed!!!
+            $scope.alertMessage = "Please select the Image to be Deleted!!";
+            return;
+        }
+
+        var result = ajaxService.ajaxHttpPostCall("./deleteImages",objectsToBeDeleted)
+        result.then(function(response){
+            $scope.disableEditMode(); //exit from edit mode
+            $scope.getImages();  // call get images to fetch latest list
+        });
+    };
+
+    $scope.enableEditMode = function(){
+        $scope.checkboxList = []
+        $scope.isEditMode = true;
+    };
+
+    $scope.disableEditMode = function(){
+        $scope.checkboxList = [];
+        $scope.isEditMode = false;
+    };
+    
+    $scope.getImages();
+}); 
